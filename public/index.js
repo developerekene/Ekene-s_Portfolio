45c645232ede1b7a2508f0fa6f7af0a891d19efc
Ekene Okoli <developerekene@gmail.com>
2022-03-04 22:01:10 +0100

    Update 1

---
+++ public/index.js
@@ -0,0 +1,49 @@
+// // document.getElementById("btn").addEventListener("click", function() {
+// //     let x = Math.floor(Math.random() * 10) + 1;
+// //     let y = document.querySelector('#para').innerHTML = x;
+// //     let b = document.querySelector('#my-input').innerHTML = "The value of y is " + y;
+
+// //   });
+
+// //To reverse a string
+// // function reverseString(string){
+// //     var splitString = string.split("");
+// //     var reverseArray = splitString.reverse();
+// //     var joinArray = reverseArray.join("");
+// //     return joinArray;
+// // }
+
+// // console.log(reverseString("Ekene"));
+
+// // let x = "Ekene";
+// // y = x.split("");
+// // console.log(y);
+// // z = y[0] + y[2] + y[4] + 5 + y[2] + 9;
+// // console.log(z);
+
+
+// // function userId(name){
+// //     //a variable to store the user name
+// //     let a = "";
+// //     //split input to individual strings 
+// //     let b = name.split("");
+// //     //
+// //     let c = b[0] + "yur" + b[2] + b[4] + "mun";
+// //     a = c;
+// //     return a;
+// // }
+// // console.log(userId("James"));
+
+// let b = [[1,2,3,4], [5,3,7,3], [4,4,2,2,5]];
+// console.log(b[0]);
+
+// // function getRandomString(length) {
+// //     var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+// //     var result = '';
+// //     for ( var i = 0; i < length; i++ ) {
+// //         result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+// //     }
+// //     return result;
+// // }
+
+// // console.log(getRandomString(""));
